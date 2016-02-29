b23975b13f40835a59d378596362f6555c43438b
benhigh22 <benjaming.high@gmail.com>
2016-02-29 15:44:07 -0500

    Better

--- calculator_app/views.py
+++ calculator_app/views.py
@@ -2,20 +2,25 @@ from django.shortcuts import render
 
 
 def index_view(request):
-    first_number = float(request.POST.get('first_number'))
-    operator = request.POST.get('operator')
-    second_number = float(request.POST.get('second_number'))
-    answer=0
-    if first_number and second_number:
-        if operator == "+":
-            answer = first_number + second_number
+    if request.method == "POST":
+        answer=0
+        first_number = float(request.POST.get('first_number'))
+        operator = request.POST.get('operator')
+        second_number = float(request.POST.get('second_number'))
 
-        elif operator == "-":
-            answer = first_number - second_number
+        if first_number and second_number:
+            if operator == "+":
+                answer = (first_number + second_number)
 
-        elif operator == "*":
-            answer = first_number * second_number
+            elif operator == "-":
+                answer = (first_number - second_number)
 
-        elif operator == "/":
-            answer = first_number / second_number
-    return render(request, 'index_view.html', {'answer': answer})
+            elif operator == "*":
+                answer = (first_number * second_number)
+
+            elif operator == "/":
+                answer = (first_number / second_number)
+
+        return render(request, 'index_view.html', {'answer': answer})
+    else:
+        return render(request, 'index_view.html')
