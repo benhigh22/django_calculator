6c85f086c1c2b13d2e94cfeb8908d3bf2b9ba6eb
benhigh22 <benjaming.high@gmail.com>
2016-02-29 16:24:49 -0500

    Normal mode working

--- calculator_app/views.py
+++ calculator_app/views.py
@@ -2,25 +2,30 @@ from django.shortcuts import render
 
 
 def index_view(request):
-    if request.method == "POST":
-        answer=0
-        first_number = float(request.POST.get('first_number'))
-        operator = request.POST.get('operator')
-        second_number = float(request.POST.get('second_number'))
+    try:
+        if request.method == "POST":
+            answer=0
+            operator = request.POST.get('operator')
+            first_number = float(request.POST.get('first_number'))
+            second_number = float(request.POST.get('second_number'))
 
-        if first_number and second_number:
-            if operator == "+":
-                answer = (first_number + second_number)
+            if first_number and second_number:
+                if operator == "+":
+                    answer = (first_number + second_number)
 
-            elif operator == "-":
-                answer = (first_number - second_number)
+                elif operator == "-":
+                    answer = (first_number - second_number)
 
-            elif operator == "*":
-                answer = (first_number * second_number)
+                elif operator == "*":
+                    answer = (first_number * second_number)
 
-            elif operator == "/":
-                answer = (first_number / second_number)
+                elif operator == "/":
+                    answer = (first_number / second_number)
 
+            return render(request, 'index_view.html', {'answer': answer, 'first_number': first_number,
+                                                       'second_number': second_number, 'operator': operator})
+        else:
+            return render(request, 'index_view.html')
+    except (ValueError, TypeError):
+        answer = "Sorry, your input must be numbers. Try again."
         return render(request, 'index_view.html', {'answer': answer})
-    else:
-        return render(request, 'index_view.html')
